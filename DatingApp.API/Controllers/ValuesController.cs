751bc186ac5168d773a2147d81ef92c3fed28c50
Shubham Jindal <shubham27jindal@gmail.com>
2020-05-04 00:36:58 +0530

    Initial commit

---
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -0,0 +1,71 @@
+﻿using DatingApp.API.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.API.Controllers
+{
+	//http:localhost:5000/api/values
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ValuesController : ControllerBase
+	{
+		private readonly AppDbContext _context;
+
+		public ValuesController(AppDbContext context)
+		{
+			_context = context;
+		}
+
+		// GET api/values
+		[HttpGet]
+		//Asynchronous code
+		public async Task<IActionResult> GetValues()
+		//public IActionResult GetValues() //Synchronous code
+		{
+			//var values = _context.Values.ToList();
+			var values = await _context.Values.ToListAsync();
+
+			return Ok(values);
+		}
+		//public ActionResult<IEnumerable<string>> Get()
+		//{
+		//	//throw new Exception("Test Exception");
+		//	return new string[] { "value1", "value2" };
+		//}
+
+		// GET api/values/5
+		[HttpGet("{id}")]
+		public async Task<IActionResult> GetValue(int id)
+		{
+			var value = await _context.Values.FirstOrDefaultAsync(x => x.Id == id);
+
+			return Ok(value);
+		}
+		//public ActionResult<string> Get(int id)
+		//{
+		//	return "value";
+		//}
+
+		// POST api/values
+		[HttpPost]
+		public void Post([FromBody] string value)
+		{
+		}
+
+		// PUT api/values/5
+		[HttpPut("{id}")]
+		public void Put(int id, [FromBody] string value)
+		{
+		}
+
+		// DELETE api/values/5
+		[HttpDelete("{id}")]
+		public void Delete(int id)
+		{
+		}
+	}
+}
---
+++ DatingApp.API/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.API.Data
+{
+	public class AppDbContext : DbContext
+	{
+		public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+		public DbSet<Value> Values { get; set; }
+	}
+}
