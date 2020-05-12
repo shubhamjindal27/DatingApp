ca5c10aaee4787fc57a71176d7bf391273747f5c
Shubham Jindal <shubham27jindal@gmail.com>
2020-05-13 00:35:31 +0530

    End of Section 8

--- DatingApp.API/Data/AppDbContext.cs
+++ DatingApp.API/Data/AppDbContext.cs
@@ -1,9 +1,5 @@
 ﻿using DatingApp.API.Models;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace DatingApp.API.Data
 {
@@ -13,5 +9,6 @@ namespace DatingApp.API.Data
 
 		public DbSet<Value> Values { get; set; }
 		public DbSet<User> Users { get; set; }
+		public DbSet<Photo> Photos { get; set; }
 	}
 }
--- DatingApp.API/Data/AuthRepository.cs
+++ DatingApp.API/Data/AuthRepository.cs
@@ -1,8 +1,5 @@
 ﻿using DatingApp.API.Models;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
